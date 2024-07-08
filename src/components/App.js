28b6aae5ad7a9076a59285478e52050efd0f21dc
shantanu400 <ashunayan400@gmail.com>
2024-07-08 22:08:25 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+import Login from "./login";
+
+
 const App = () => {
+
+  const[isLoggedIn,setisLoggedIn]=useState(false);
+
+const changeState=()=>{
+  setisLoggedIn(true);
+  console.log("in chang state");
+ 
+}
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       
+       {isLoggedIn ? <p>You Are Loggedin!</p> : <Login func={changeState}/>}
+        
     </div>
   )
 }
---
+++ src/components/Login.js
@@ -0,0 +1,31 @@
+import React from "react"
+export default function Login(props){
+    return (
+    
+
+
+        <div className="roots">
+
+       
+        <div data-mdb-input-init className="form-outline mb-4">
+          <input type="email" id="loginName" className="form-control" />
+          <label className="form-label" htmlFor="loginName">Email or username</label>
+        </div>
+  
+        
+        <div data-mdb-input-init className="form-outline mb-4">
+          <input type="password" id="loginPassword" className="form-control" />
+          <label className="form-label" htmlFor="loginPassword">Password</label>
+        </div>
+  
+    
+      
+          <div className="col-md-6 d-flex justify-content-center">
+      
+        <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary btn-block mb-4" onClick={()=>props.func()}>Sign in</button>
+          
+      </div>
+      
+      </div>
+    )
+}
